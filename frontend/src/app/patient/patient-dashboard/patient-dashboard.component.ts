8a06d542f7b0530d25eaee4fec4a9a926cf7706c
faisal-fida <arainfaisal826@gmail.com>
2023-08-26 11:47:18 +0500

    Faisal

--- frontend/src/app/patient/patient-dashboard/patient-dashboard.component.ts
+++ frontend/src/app/patient/patient-dashboard/patient-dashboard.component.ts
@@ -1,38 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { PatientRouteService } from '../patient-services/patientroute.service';
 import { HttpClient } from '@angular/common/http';
-import { Patient } from '../patient.model';
+import { PatientDashboard } from './patient-dashboard.model';
 
 @Component({
   selector: 'app-patient-dashboard',
   templateUrl: './patient-dashboard.component.html',
   styleUrls: ['./patient-dashboard.component.css']
 })
-export class PatientDashboardComponent implements OnInit  {
+export class PatientDashboardComponent implements OnInit {
+  patientData: PatientDashboard;
 
-  constructor(private patroute:PatientRouteService,private http:HttpClient){}
+  constructor(private http: HttpClient) {}
 
-  patients:any[]=[];
+  ngOnInit(): void {
+    this.fetchPatientData();
+  }
 
-// ngOnInit() {
-//   // this.patroute.getPatients().subscribe(data=>{
-//   //      this.patients=data;
-//   //      console.log(this.patients);
-
-//   this.patroute.getPatients();
-  
-//   }
-
-ngOnInit(){
-  this.getPatients();
+  fetchPatientData() {
+    this.http.get<PatientDashboard>('http://127.0.0.1:8000/patients/1/').subscribe(data => {
+      this.patientData = data;
+    });
+  }
 }
-
-  ComingPatients:Patient;
-
-getPatients() 
-{
-  this.http.get('http://127.0.0.1:8000/patients/1').subscribe((patient:Patient) => { this.ComingPatients=patient; });
-}
-  
-}
-

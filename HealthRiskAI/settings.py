27819713486fe37d01098eea7dd1a9e2bc8049d4
faisal-fida <arainfaisal826@gmail.com>
2023-08-29 12:22:21 +0500

    postgre

--- HealthRiskAI/settings.py
+++ HealthRiskAI/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'HealthRiskAI.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todoapp',
+        'USER': 'faisal',
+        'PASSWORD': '3JX7S1cw46D6YwNrUHmh',
+        'HOST': 'djangodb.c83dxhl2wyjt.ap-southeast-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
